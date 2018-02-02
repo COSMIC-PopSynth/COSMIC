8209f8ddb013b0174a890de8174946b673aa4097
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-02-02 15:33:46 -0600

    changing around initial samples to have a single function per parameter with multiple models per function

--- aCOSMIC/sample.py
+++ aCOSMIC/sample.py
@@ -53,42 +53,63 @@ class Sample:
 
 
     # sample primary masses
-    def sample_kroupa93(self, kstar1_final, size=None):
+    def sample_primary(self, kstar1_final, model='kroupa93', size=None):
         '''
-        Primary mass follows Kroupa (1993), normalization comes from
+        kroupa93 follows Kroupa (1993), normalization comes from
         `Hurley (2002) <https://arxiv.org/abs/astro-ph/0201220>`_ between 0.1 and 100 Msun
+        salpter55 follows Salpeter (1955) <http://adsabs.harvard.edu/abs/1955ApJ...121..161S>`_ between 0.1 and 100 Msun
         '''
 
-        # If the final binary contains a compact object (BH or NS),
-        # we want to evolve 'size' binaries that could form a compact
-        # object so we over sample the initial population
-        if kstar1_final > 12.0:
-            a_0 = np.random.uniform(0.0, 0.9999797, size*500)
-        else:   
-            a_0 = np.random.uniform(0.0, 0.9999797, size)
-
+        if model=='kroupa93':
+            # If the final binary contains a compact object (BH or NS),
+            # we want to evolve 'size' binaries that could form a compact
+            # object so we over sample the initial population
+            if kstar1_final > 14.0:
+                a_0 = np.random.uniform(0.0, 0.9999797, size*500)
+            elif kstar1_final > 12.0:
+                a_0 = np.random.uniform(0.0, 0.9999797, size*50)
+            else:   
+                a_0 = np.random.uniform(0.0, 0.9999797, size)
+            
+            low_cutoff = 0.740074
+            high_cutoff=0.908422
+     
+            lowIdx, = np.where(a_0 <= low_cutoff)
+            midIdx, = np.where((a_0 > low_cutoff) & (a_0 < high_cutoff)) 
+            highIdx, = np.where(a_0 >= high_cutoff)
+     
+            a_0[lowIdx] = ((0.1) ** (-3.0/10.0) - (a_0[lowIdx] / 0.968533)) ** (-10.0/3.0)
+            a_0[midIdx] = ((0.5) ** (-6.0/5.0) - ((a_0[midIdx] - low_cutoff) / 0.129758)) ** (-5.0/6.0)
+            a_0[highIdx] = (1 - ((a_0[highIdx] - high_cutoff) / 0.0915941)) ** (-10.0/17.0)
+            
+            total_sampled_mass = np.sum(a_0)
+            if kstar1_final > 13.0:
+                a_0 = a_0[a_0 > 15.0]
+            elif kstar1_final > 12.0: 
+                a_0 = a_0[a_0 > 8.0]
+            return a_0, total_sampled_mass
         
-        low_cutoff = 0.740074
-        high_cutoff=0.908422
+        elif model=='salpeter55':
+            # If the final binary contains a compact object (BH or NS),
+            # we want to evolve 'size' binaries that could form a compact
+            # object so we over sample the initial population
+            if kstar1_final > 14.0:
+                mSamp = np.random.power(-1.3, size*500)
+            elif kstar1_final > 12.0:
+                mSamp = np.random.power(-1.3, size*50)
+            else:
+                mSamp = np.random.power(-1.3, size*500)
+            
+            # Transform sample from 0 to 1 to be between 0.1 and 100 Msun
+            a_0 = mSamp*(100.0-0.1)+0.1
 
-
-        lowIdx, = np.where(a_0 <= low_cutoff)
-        midIdx, = np.where((a_0 > low_cutoff) & (a_0 < high_cutoff)) 
-        highIdx, = np.where(a_0 >= high_cutoff)
-
-        a_0[lowIdx] = ((0.1) ** (-3.0/10.0) - (a_0[lowIdx] / 0.968533)) ** (-10.0/3.0)
-        a_0[midIdx] = ((0.5) ** (-6.0/5.0) - ((a_0[midIdx] - low_cutoff) / 0.129758)) ** (-5.0/6.0)
-        a_0[highIdx] = (1 - ((a_0[highIdx] - high_cutoff) / 0.0915941)) ** (-10.0/17.0)
-        
-        total_sampled_mass = np.sum(a_0)
-        if kstar1_final > 13.0:
-            a_0 = a_0[a_0 > 20.0]
-        elif kstar1_final > 12.0: 
-            a_0 = a_0[a_0 > 10.0]
-        print 'number of primary stars', len(a_0)     
-        return a_0, total_sampled_mass
-
-        
+            total_sampled_mass = np.sum(a_0)
+            if kstar1_final > 13.0:
+                a_0 = a_0[a_0 > 15.0]
+            elif kstar1_final > 12.0:
+                a_0 = a_0[a_0 > 8.0]
+            return a_0, total_sampled_mass 
+                   
     # sample secondary mass
     def sample_secondary(self, primary_mass):
         '''
@@ -103,17 +124,19 @@ class Sample:
         return secondary_mass
 
 
-    def binary_select_vanHaaften(self, primary_mass):
+    def binary_select(self, primary_mass, model='vanHaaften'):
         '''
         Binary fraction is set by `van Haaften et al.(2009)<http://adsabs.harvard.edu/abs/2013A%26A...552A..69V>`_ in appdx
         '''
-        binary_fraction = 1/2.0 + 1/4.0 * np.log10(primary_mass)
-        binary_choose =  np.random.uniform(0, 1.0, binary_fraction.size)
-
-        binaryIdx, = np.where(binary_fraction > binary_choose)
-        singleIdx, = np.where(binary_fraction < binary_choose)
-
-        return primary_mass[binaryIdx], primary_mass[singleIdx]
+        
+        if model=='vanHaaften':
+            binary_fraction = 1/2.0 + 1/4.0 * np.log10(primary_mass)
+            binary_choose =  np.random.uniform(0, 1.0, binary_fraction.size)
+     
+            binaryIdx, = np.where(binary_fraction > binary_choose)
+            singleIdx, = np.where(binary_fraction < binary_choose)
+     
+            return primary_mass[binaryIdx], primary_mass[singleIdx]
 
     def sep_to_porb(self, mass1, mass2, sep):
         '''
@@ -125,74 +148,67 @@ class Sample:
 
         return porb_sec
 
-    def sample_porb_Han(mass1, mass2, size=None):
+    def sample_porb(mass1, mass2, model='Han', size=None):
         '''
-        Separation is sampled according to `Han (1998)<http://adsabs.harvard.edu/abs/1998MNRAS.296.1019H>`_
+        If model='Han', separation is sampled according to `Han (1998)<http://adsabs.harvard.edu/abs/1998MNRAS.296.1019H>`_
+        If model='log_normal', Use (ref from Aaron Geller) to sample a log normal distributed orbital period
+        with mu=1e5.03 days and sigma=1e2.28 days; converts period from days to seconds.
         Separation is then converted to orbital period in seconds
         '''
-
-        a_0 = np.random.uniform(0, 1, size)
-        low_cutoff = 0.0583333
-        lowIdx, = np.where(a_0 <= low_cutoff)
-        hiIdx, = np.where(a_0 > low_cutoff)
         
-        a_0[lowIdx] = (a_0[lowIdx]/0.00368058)**(5/6.0)
-        a_0[hiIdx] = np.exp(a_0[hiIdx]/0.07+math.log(10.0))
-        
-        # convert to meters
-        a_0 = a_0*Rsun
-        
-        porb_sec = sep_to_porb(mass1, mass2, a_0)
-        return porb_sec
-
-
-    def sample_porb_lognormal(self, size=None):
-        '''
-        Use (ref from Aaron Geller) to sample a log normal distributed orbital period
-        with mu=1e5.03 days and sigma=1e2.28 days; converts period from days to seconds.
-        '''
-
-        tb = np.random.lognormal(mean=1e5.03, sigma=1e2.28, size=size)
-
-        return tb*sec_in_day
-
-
-    def sample_thermal_ecc(self, size=None):
-        '''
-        Thermal eccentricity distribution following `Heggie (1975)<http://adsabs.harvard.edu/abs/1975MNRAS.173..729H>`_ 
-        '''
-        
-        a_0 = np.random.uniform(0.0, 1.0, size)
- 
-        return a_0**0.5
-
-    def sample_thermal_ecc(self, size=None):
-        '''
-        Sample eccentricities uniformly between 0 and 1 following ref ref from Aaron Geller
-        '''
-        
-        ecc = np.random.uniform(0.0, 1.0, size)
-
-        return ecc
-
-
-    def sample_constant_SFH(self, t_component, size=None):
-        '''
-        Assign an evolution time assuming a constant star formation rate over the age: t_component which is specified in [Myr]
-        '''
-
-        tphys = np.random.uniform(0, t_component, size)
-        return tphys
-
+        if model=='Han': 
+            a_0 = np.random.uniform(0, 1, size)
+            low_cutoff = 0.0583333
+            lowIdx, = np.where(a_0 <= low_cutoff)
+            hiIdx, = np.where(a_0 > low_cutoff)
+            
+            a_0[lowIdx] = (a_0[lowIdx]/0.00368058)**(5/6.0)
+            a_0[hiIdx] = np.exp(a_0[hiIdx]/0.07+math.log(10.0))
+            
+            # convert to meters
+            a_0 = a_0*Rsun
+            
+            porb_sec = sep_to_porb(mass1, mass2, a_0)
+            return porb_sec
      
-    def sample_gyr_burst(self, t_component, size=None):
+        if model=='log_normal':
+            porb = np.random.lognormal(mean=1e5.03, sigma=1e2.28, size=size)
+
+            return porb*sec_in_day
+
+
+    def sample_ecc(self, model='thermal', size=None):
         '''
-        Assign an evolution time assuming constant star formation rate for 1Gyr starting at 't_component' Myr in the past
+        If model=='thermal', thermal eccentricity distribution following `Heggie (1975)<http://adsabs.harvard.edu/abs/1975MNRAS.173..729H>`_ 
+        
+        If model=='uniform', Sample eccentricities uniformly between 0 and 1 following ref ref from Aaron Geller '''
+        
+        if model=='thermal':
+            a_0 = np.random.uniform(0.0, 1.0, size)
+ 
+            return a_0**0.5
+
+        if model=='uniform':
+            ecc = np.random.uniform(0.0, 1.0, size)
+
+            return ecc
+
+
+    def sample_SFH(self, model='const', size=None):
+        '''
+        'const': Assign an evolution time assuming a constant star formation rate over the age of the MW disk: 10,000 [Myr]
+        'burst': Assign an evolution time assuming constant star formation rate for 1Gyr starting at 't_component' 10,000 [Myr] in the past
         '''
 
-        tphys = t_component - np.random.uniform(0, 1000, size)
-        return tphys
+        if model='const':
 
+            tphys = np.random.uniform(0, 10000.0, size)
+            return tphys
+ 
+        if model='burst':
+            tphys = 10000.0 - np.random.uniform(0, 1000, size)
+            return tphys
+     
      
     def set_kstar(self, mass):
         '''
