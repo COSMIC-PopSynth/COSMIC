c8847583747f20c62d7bb6daaa43cbf5a1513e72
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-21 12:42:31 -0600

    sample primary covered

--- aCOSMIC/tests/test_sample.py
+++ aCOSMIC/tests/test_sample.py
@@ -14,11 +14,10 @@ TEST_DATA_DIR = os.path.join(os.path.split(__file__)[0], 'data')
 
 f  = np.linspace(0,1,10)
 x = np.linspace(0,1,10)
-np.random.seed(2)
-
 
 IDL_TABULATE_ANSWER = 0.5
-TOTAL_SAMPLED_MASS = 2.138821630578966 
+TOTAL_SAMPLED_MASS_KROUPA93 = 2.138821630578966 
+TOTAL_SAMPLED_MASS_SALPETER55 = 1.4513692733524741
 
 class TestSample(unittest2.TestCase):
     """`TestCase` for the aCOSMIC
@@ -33,11 +32,13 @@ class TestSample(unittest2.TestCase):
         self.assertAlmostEqual(idl_tabulate(x,f), IDL_TABULATE_ANSWER)
 
     def test_sample_primary_kroupa93(self):
+        np.random.seed(2)
         # Check that the sample_primary function samples mass correctly
         a_0, total_sampled_mass = tmp.sample_primary(10, size=10)
-        self.assertEqual(total_sampled_mass, TOTAL_SAMPLED_MASS)
+        self.assertEqual(total_sampled_mass, TOTAL_SAMPLED_MASS_KROUPA93)
 
     def test_sample_primary_salpeter55(self):
+        np.random.seed(2)
         # Check that the sample_primary function samples mass correctly
         a_0, total_sampled_mass = tmp.sample_primary(kstar1_final=10, model='salpeter55', size=10)
-        self.assertEqual(total_sampled_mass, TOTAL_SAMPLED_MASS)
+        self.assertEqual(total_sampled_mass, TOTAL_SAMPLED_MASS_SALPETER55)
