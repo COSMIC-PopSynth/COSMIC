a331374ada21ca6f478b07662b99d14e94bfc001
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-21 10:49:01 -0600

    Let us check on coveralls now

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Accretion w/ COSMIC
 
 [![Build Status](https://travis-ci.org/aCOSMIC/aCOSMIC.svg?branch=master)](https://travis-ci.org/aCOSMIC/aCOSMIC)
+
+[![Coverage Status](https://coveralls.io/repos/github/aCOSMIC/aCOSMIC/badge.svg?branch=master)](https://coveralls.io/github/aCOSMIC/aCOSMIC?branch=master)
--- aCOSMIC/tests/test_sample.py
+++ aCOSMIC/tests/test_sample.py
@@ -5,9 +5,21 @@ __author__ = 'Katie Breivik <katie.breivik@gmail.com>'
 
 import os
 import unittest2
+import numpy as np
+f  = np.linspace(0,1,10)
+x = np.linspace(0,1,10)
+import scipy.integrate
 
+
+
+IDL_TABULATE_ANSWER = 0.5
 class TestSample(unittest2.TestCase):
     """`TestCase` for the aCOSMIC
     """
+    def test_idl_tabulate(self):
+        # Give this custom integrator a simple integration
+        # of a line from x = 0 to 1 and y= 0 to 1
+        from aCOSMIC.sample import idl_tabulate
+        self.assertAlmostEqual(idl_tabulate(x,f), IDL_TABULATE_ANSWER)
     def test_sample_masses(self):
         self.assertEqual(1, 1)
