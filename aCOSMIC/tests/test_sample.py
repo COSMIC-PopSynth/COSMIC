c52b531e2b92438b8081480adfb7861bb236d515
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-02-21 10:13:09 -0600

    Lets try a unit test

---
+++ aCOSMIC/tests/test_sample.py
@@ -0,0 +1,13 @@
+"""Unit test for aCOSMIC
+"""
+
+__author__ = 'Katie Breivik <katie.breivik@gmail.com>'
+
+import os
+import unittest2
+
+class TestSample(unittest2.TestCase):
+    """`TestCase` for the aCOSMIC
+    """
+    def test_sample_masses(self):
+        self.assertEqual(1, 1)
