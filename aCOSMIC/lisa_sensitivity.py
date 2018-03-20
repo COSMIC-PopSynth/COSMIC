84212d5446f0d3734d7860025a52951d293cc6b1
BREIVIK Katelyn <kmb799@quser10.quest.it.northwestern.edu>
2018-03-19 19:57:27 -0500

    lowering lower bound on sensitivity curve

--- aCOSMIC/lisa_sensitivity.py
+++ aCOSMIC/lisa_sensitivity.py
@@ -935,7 +935,7 @@ def lisa_root_psd():
     '''Computes LISA sensitivity curve according to `Cornish and Robson 2018 <https://arxiv.org/pdf/1803.01944.pdf>`_
     ''' 
 
-    freq = np.logspace(-6,1)
+    freq = np.logspace(-7,1)
     # note: freq [Hz], L_arm [m], S_n [Hz^-0.5]
     L_arm = 2.5e9
     f_star = 19.09*1e-3
