0398b7340b115c0c5680aaceefad4b57bb5766d2
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-08-05 20:52:44 -0500

    fixing deleted data needed for tests

--- cosmic/tests/test_MC_samp.py
+++ cosmic/tests/test_MC_samp.py
@@ -15,15 +15,15 @@ y_sun = 0.0
 z_sun = 25
 
 TEST_DATA_DIR = os.path.join(os.path.split(__file__)[0], 'data')
-FIXED_POP = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'dat_ThinDisk_11_11.h5'), key='bcm')
-MASS_FIXED = np.max(pd.read_hdf(os.path.join(TEST_DATA_DIR, 'dat_ThinDisk_11_11.h5'), key='totalMass'))
+FIXED_POP = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'dat_ThinDisk_11_11_3.h5'), key='bcm')
+MASS_FIXED = np.max(pd.read_hdf(os.path.join(TEST_DATA_DIR, 'dat_ThinDisk_11_11_3.h5'), key='totalMass'))
 GX_COMPONENT_THIN = 'ThinDisk'
 GX_COMPONENT_THICK = 'ThickDisk'
 GX_COMPONENT_BULGE = 'Bulge'
 GX_COMPONENT_MASS_THIN = 4.32e10
 GX_COMPONENT_MASS_THICK = 1.44e10
 GX_COMPONENT_MASS_BULGE = 8.9e9
-N_GX = 894851193
+N_GX = 135840169
 
 class TestMC_samp(unittest2.TestCase):
     """`TestCase` for the MC samp method
