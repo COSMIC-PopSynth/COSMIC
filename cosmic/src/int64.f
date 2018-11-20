78cef1ecdb3f1b3ccba045b196e1bdea8908ad62
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-11-20 10:13:53 -0600

    This si one way that having bse in its own repo can work with cosmic

--- cosmic/src/int64.f
+++
@@ -1,4 +0,0 @@
-      BLOCK DATA integers_init
-        include "int64.h"
-        DATA umax /z'ffffffff'/
-      END
--- cosmic/src/taus113tester.f
+++
@@ -1,6 +0,0 @@
-      program taus113_tester
-        external test_taus
-
-        call test_taus()
-        call taus113_ran3_tester()
-      end program
