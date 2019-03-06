7bfc36a2286951024cd166542679542330f17034
michaelzevin <mzevin1@gmail.com>
2019-03-06 17:03:05 -0600

    fixed polar angle kicks in kick.f. added error for polar_kick_angles not between 0-90. wrote code in kick.f for determining the total tilt of the orbital plane and added this to the bkick array.

--- cosmic/src/kick.f
+++ cosmic/src/kick.f
@@ -6,6 +6,7 @@
 * Updated JRH kick routine by PDK (see Kiel & Hurley 2009).
 *
 * Here theta is the \omega angle within the HTP02 paper (thus phi is phi).
+* Also, mu is the \nu angle in the HTP02 paper and omega its azimuth
 *
 * Produces a random SN kick.
 * Evolution was added for binaries in which the kick creates
@@ -22,7 +23,7 @@
 * bkick[10-12] to star 1. Here, star 1 is the star that was passed first to
 * evolv1.f or evolv2.f.
 *
-* MZ/SC/KK: bkick[13-20] specify information about the SN and how
+* MJZ/SC/KK: bkick[13-20] specify information about the SN and how
 * it affected the orbit. bkick[13,14] give the magnitude of the
 * natal kicks from the first and second SN. bkick[15-17] give
 * the change in the systemic velocity from the first SN (15),
@@ -30,7 +31,8 @@
 * *both* SN (17). bkick[18-20] give the angular change of the
 * orbital angular momentum vector from the first SN (18),
 * second SN (19), and the total change before and after *both*
-* SN (20).
+* SN (20). Note that the total change in the orbital plane
+* doesn't use a self-consistent value of omega
 *
 * Add tphys to input params, both above and within evolv2.f
 * bkick(5) was made negative in the bse_interface routine
@@ -48,13 +50,15 @@
       real*8 m1,m2,m1n,mbi,mbf,mdif
       real*8 ecc,sep,sepn,jorb,ecc2
       real*8 pi,twopi,gmrkm,yearsc,rsunkm
-      real*8 cosmu,sinmu,z_tilt
       parameter(yearsc=3.1557d+07,rsunkm=6.96d+05)
       real*8 mm,em,dif,der,del,r
       real*8 u1,u2,vk,v(4),s,theta,phi
       real*8 sphi,cphi,stheta,ctheta,salpha,calpha
+      real*8 x_tilt,y_tilt,z_tilt
+      real*8 mu,cmu,smu,omega,comega,somega
+      real*8 cmu1,smu1,comega1,somega1
       real*8 vr,vr2,vk2,vn2,hn2
-      real*8 mu,cmu,vs(3),v1,v2
+      real*8 vs(3),v1,v2
       real*8 mx1,mx2,r2
       real*8 sigma,sigmah,bhsigmafrac,RotInvX
       real*8 signs,sigc,psins,psic,cpsins,spsins,cpsic,spsic
@@ -66,9 +70,9 @@
       real*8 v2xout,v2yout,v2zout
       logical output
 *
-      real*8 bconst,CK,polar_kick_angle,cmu_SN1
+      real*8 bconst,CK,polar_kick_angle,mu_SN1,omega_SN1
       COMMON /VALUE4/ sigma,bhsigmafrac,bconst,CK
-      COMMON /VALUE4/ polar_kick_angle,cmu_SN1,bhflag
+      COMMON /VALUE4/ polar_kick_angle,mu_SN1,omega_SN1,bhflag
       real*8 mxns,neta,bwind,hewind
       COMMON /VALUE1/ neta,bwind,hewind,mxns
       real*8 bkick(20)
@@ -98,7 +102,7 @@
       kickscale = 0.d0
 *
 *
-      if(sigma.lt.0.d0)then 
+      if(sigma.lt.0.d0)then
          sigma = -1.d0*sigma
          kickscale = 10.d0
       endif
@@ -217,7 +221,7 @@
           elseif(kw.eq.14.and.bhflag.eq.2)then
              vk = vk * mxns / m1n
              vk2 = vk*vk
-          endif 
+          endif
       endif
 
 * Set natal kick magnitude in the bkick array
@@ -228,27 +232,18 @@
       endif
 
       sigma = sigmah
-* Before we randomly drawa phi and theta,
+
+
+* Before we randomly draw a phi and theta,
 * let us see if a pre-supplied set of natal kicks
-* and phi theta values associated with the kicks was passed.
+* and phi/theta values associated with the kicks was passed.
       if(natal_kick(snstar).gt.0.d0)then
           phi = natal_kick(snstar+2)
           sphi = SIN(phi)
           cphi = COS(phi)
-          if(bkick(1).gt.0.d0.and.bkick(5).le.0.d0)then
-            cosmu = cmu_SN1
-            sinmu = sqrt(1 - cmu_SN1*cmu_SN1)
-*           this stheta should probably be from first kick
-            z_tilt=cosmu*sphi + cphi*sinmu*stheta
-            phi = ASIN(z_tilt)
-            sphi = z_tilt
-            cphi = COS(phi)
-*           write(15,*) vk,theta,phi,cmu_SN1
-          endif
           theta = natal_kick(snstar+4)
           stheta = SIN(theta)
           ctheta = COS(theta)
-       
       else
 * CLR - Allow for a restricted opening angle for SN kicks
 *       Only relevant for binaries, obviously
@@ -257,31 +252,44 @@
           sphi = (1.d0-bound)*ran3(idum) + bound
           phi = ASIN(sphi)
 * MJZ - The constrained kick will hit at either the north
-*       or south pole
+*       or south pole, so randomly choose the hemisphere
           if(RAN3(idum).ge.0.5)then
             phi = -phi
             sphi = SIN(phi)
           endif
           cphi = COS(phi)
-
-* CLR - if the orbit has already been kicked, then any polar kick
-*       needs to be tilted as well (since L_hat and S_hat are no longer
-*       aligned).  Here we take the random kick from above and rotate it
-*       about the X axis by the angle mu from the last SN kick
-          if(bkick(1).gt.0.d0.and.bkick(5).le.0.d0)then
-            cosmu = cmu_SN1
-            sinmu = sqrt(1 - cmu_SN1*cmu_SN1)
-*           this stheta should probably be from first kick
-            z_tilt=cosmu*sphi + cphi*sinmu*stheta
-            phi = ASIN(z_tilt)
-            sphi = z_tilt
-            cphi = COS(phi)
-*           write(15,*) vk,theta,phi,cmu_SN1
-          endif
           theta = twopi*ran3(idum)
           stheta = SIN(theta)
           ctheta = COS(theta)
       endif
+
+* CLR - if the orbit has already been kicked, then any polar kick
+*       needs to be tilted as well (since L_hat and S_hat are no longer
+*       aligned).
+* MJZ - to track the total angular change in the binary's orbital plane,
+*       we use both the value of \mu and \omega from the first SN.
+*       We first rotate by \mu about the x-axis, then by \omega
+*       about the z-axis.
+      if(bkick(1).gt.0.d0.and.bkick(5).le.0.d0)then
+        cmu = COS(mu_SN1)
+        smu = SIN(mu_SN1)
+        comega = COS(omega_SN1)
+        somega = SIN(omega_SN1)
+
+        x_tilt = ctheta*cphi*comega + smu*sphi*somega -
+     &                   cmu*cphi*stheta*somega
+        y_tilt = cmu*cphi*comega*stheta + ctheta*cphi*somega -
+     &                   comega*smu*sphi
+        z_tilt = cmu*sphi + cphi*smu*stheta
+
+        phi = ASIN(z_tilt)
+        sphi = z_tilt
+        cphi = COS(phi)
+        theta = ATAN(y_tilt/x_tilt)
+        stheta = SIN(theta)
+        ctheta = COS(theta)
+      endif
+
 
       if(sep.le.0.d0.or.ecc.lt.0.d0) goto 90
 *
@@ -305,18 +313,42 @@
 * hn to units of Rsun^2/yr.
       jorb = (m1n*m2/(m1n+m2))*SQRT(hn2)*(yearsc/rsunkm)
 * Determine the angle between the new and old orbital angular
-* momentum vectors.
+* momentum vectors, and randomly choose an azimuth angle
       cmu = (vr*salpha-vk*ctheta*cphi)/SQRT(v1 + v2)
+      mu = ACOS(cmu)
+      smu = SIN(mu)
+      omega = twopi*ran3(idum)
+      comega = COS(omega)
+      somega = SIN(omega)
 
-* Set angle between orbital angular momentum vectors in the bkick array
+* Set angles between orbital angular momentum vectors in the bkick array
       if(bkick(1).le.0.d0)then
-        bkick(18) = ACOS(cmu)*180/pi
-        cmu_SN1 = cmu
+        bkick(18) = mu*180/pi
+        mu_SN1 = mu
+        omega_SN1 = omega
       elseif(bkick(5).le.0.d0)then
-        bkick(19) = ACOS(cmu)*180/pi
+        bkick(19) = mu*180/pi
+
+* MJZ - Here we calculate the total change in the orbital plane
+*       from both SN. Note that these angles mu and omega are in
+*       typical spherical coordinates rather than colateral coordinates,
+*       so the rotations are slightly different than above.
+*       We rotate about z-axis by omega1 then y-axis by mu1
+        comega1 = COS(omega_SN1)
+        somega1 = SIN(omega_SN1)
+        cmu1 = COS(mu_SN1)
+        smu1 = SIN(mu_SN1)
+
+        x_tilt = cmu1*comega*comega1*smu + cmu*smu1
+     &                - cmu1*smu*somega*somega1
+        y_tilt = comega1*smu*somega + comega*smu*somega1
+        z_tilt = cmu*cmu1 + smu*smu1*somega*somega1
+     &               - comega*comega1*smu*smu1
+
+        bkick(20) = ACOS(z_tilt)*180/pi
+
       endif
 
-      mu = ACOS(cmu)
 * Determine if orbit becomes hyperbolic.
  90   continue
       mx1 = vk*m1n/(m1n+m2)
@@ -346,8 +378,8 @@
 * Calculate the velocity magnitude at infinity for hyperbolic orbit.
          v1 = SQRT((gmrkm*(m1n+m2))/sepn)
          signs = ACOS(MIN(1.d0,csigns))
-         sigc = signs 
-* Calculating position of NS compared to companion for 
+         sigc = signs
+* Calculating position of NS compared to companion for
 * rotation calculation around the z-axis.
          semilatrec = gmrkm*(m1n+m2)
          semilatrec = hn2/semilatrec
@@ -355,7 +387,7 @@
          angleofdeath = ACOS(MIN(1.d0,cangleofdeath))
 *         if(kw.eq.14) write(*,*)'0.5: ',angleofdeath,ctheta
 * Find which hemisphere (in x) the NS/companion originated in...
-         psins = angleofdeath 
+         psins = angleofdeath
          if((stheta*cphi - calpha).gt.0.d0) psins = -psins
          psic = psins
 * Accounting for rotation in x-y plane due to SN event.
@@ -365,7 +397,7 @@
          spsic = SIN(sigc-psic)
 *         if(kw.eq.14) write(*,*)'1: ',semilatrec,r,cangleofdeath,
 *     &        angleofdeath,sigc,psic,cpsic,spsic
-* Inverse rotation matrix accounting for our assumed alignment of 
+* Inverse rotation matrix accounting for our assumed alignment of
 * the post-SN orbital angular mometum with the z-axis.
 *         RotInvX = vr*salpha + vk*(sphi - ctheta*cphi)
 *         mbi = RotInvX
@@ -373,34 +405,34 @@
 *         RotInvX = RotInvX/SQRT(RotInvX*RotInvX + RotInvZ*RotInvZ)
 *         RotInvZ = RotInvZ/SQRT(mbi*mbi + RotInvZ*RotInvZ)
          RotInvX = cmu
-*     
+*
          mbi = m1+m2
          mbf = m1n+m2
          mdif = m1 - m1n
 * Energy calculation, for interest - should be positive for unbound system.
          energy = vn2/2.d0 - gmrkm*(m1n+m2)/r
-*     
+*
          if(bkick(1).le.0.d0)then
             bkick(1) = float(snstar)
-*     
-            bkick(2) = ((m1n/mbf)*vk*ctheta*cphi + 
+*
+            bkick(2) = ((m1n/mbf)*vk*ctheta*cphi +
      &           (mdif*m2)/(mbi*mbf)*vr*salpha) -
      &           v1*(spsins)*RotInvX*(m2/mbf)
-*     
-            bkick(3) = (m1n/mbf)*vk*stheta*cphi + 
-     &           (mdif*m2)/(mbi*mbf)*vr*calpha - 
+*
+            bkick(3) = (m1n/mbf)*vk*stheta*cphi +
+     &           (mdif*m2)/(mbi*mbf)*vr*calpha -
      &           v1*(cpsins)*(m2/mbf)
-*     
+*
             bkick(4) = (m1n/mbf)*vk*sphi
-*     
+*
             bkick(5) = float(snstar)
-*     
-            bkick(6) = ((mdif*m2)/(mbi*mbf)*vr*salpha + 
-     &           (m1n/mbf)*vk*ctheta*cphi) + 
+*
+            bkick(6) = ((mdif*m2)/(mbi*mbf)*vr*salpha +
+     &           (m1n/mbf)*vk*ctheta*cphi) +
      &           v1*(spsic)*(m1n/mbf)*RotInvX
-*     
-            bkick(7) = (mdif*m2)/(mbi*mbf)*vr*calpha + 
-     &           (m1n/mbf)*vk*stheta*cphi + 
+*
+            bkick(7) = (mdif*m2)/(mbi*mbf)*vr*calpha +
+     &           (m1n/mbf)*vk*stheta*cphi +
      &           v1*(cpsic)*(m1n/mbf)
 *
             bkick(8) = (m1n/mbf)*vk*sphi
@@ -426,27 +458,27 @@
             vkout2 = sqrt(v2xout*v2xout+v2yout*v2yout+v2zout*v2zout)
          elseif(bkick(1).gt.0.d0.and.bkick(5).le.0.d0)then
             bkick(5) = float(snstar)
-*     
-            bkick(6) = ((m1n/mbf)*vk*ctheta*cphi + 
+*
+            bkick(6) = ((m1n/mbf)*vk*ctheta*cphi +
      &           (mdif*m2)/(mbi*mbf)*vr*salpha) -
      &           v1*(spsins)*RotInvX*(m2/mbf)
-*     
-            bkick(7) = (m1n/mbf)*vk*stheta*cphi + 
-     &           (mdif*m2)/(mbi*mbf)*vr*calpha - 
+*
+            bkick(7) = (m1n/mbf)*vk*stheta*cphi +
+     &           (mdif*m2)/(mbi*mbf)*vr*calpha -
      &           v1*(cpsins)*(m2/mbf)
 *
             bkick(8) = (m1n/mbf)*vk*sphi
-*     
+*
             bkick(9) = float(snstar)
-*     
-            bkick(10) = ((mdif*m2)/(mbi*mbf)*vr*salpha + 
-     &           (m1n/mbf)*vk*ctheta*cphi) + 
+*
+            bkick(10) = ((mdif*m2)/(mbi*mbf)*vr*salpha +
+     &           (m1n/mbf)*vk*ctheta*cphi) +
      &           v1*(spsic)*(m1n/mbf)*RotInvX
-*     
-            bkick(11) = (mdif*m2)/(mbi*mbf)*vr*calpha + 
-     &           (m1n/mbf)*vk*stheta*cphi + 
+*
+            bkick(11) = (mdif*m2)/(mbi*mbf)*vr*calpha +
+     &           (m1n/mbf)*vk*stheta*cphi +
      &           v1*(cpsic)*(m1n/mbf)
-*     
+*
             bkick(12) = m1n/mbf*vk*sphi
 *
             if(psins.lt.0.d0)then
@@ -468,7 +500,7 @@
             v2zout = bkick(8)
             vkout1 = sqrt(v1xout*v1xout+v1yout*v1yout+v1zout*v1zout)
             vkout2 = sqrt(v2xout*v2xout+v2yout*v2yout+v2zout*v2zout)
-*     
+*
          endif
          ecc = MIN(ecc,99.99d0)
       endif
@@ -627,39 +659,39 @@
       vz3s = vz3
 * theta = b, phi = a, g = psi; pitch-roll-yaw; z-y-x axis rotations
       vx1 = vx1s*(cb*ca)
-      vx1 = vx1 + vy1s*(cb*sa) 
+      vx1 = vx1 + vy1s*(cb*sa)
       vx1 = vx1 - vz1s*sb
-*     
+*
       vy1 = vx1s*(sg*sb*ca - cg*sa)
       vy1 = vy1 + vy1s*(sg*sb*sa + cg*ca)
       vy1 = vy1 + vz1s*(cb*sg)
-*     
+*
       vz1 = vx1s*(cg*sb*ca + sg*sa)
       vz1 = vz1 + vy1s*(cg*sb*sa - sg*ca)
       vz1 = vz1 + vz1s*(cb*cg)
 **
 **
       vx2 = vx2s*(cb*ca)
-      vx2 = vx2 + vy2s*(cb*sa) 
+      vx2 = vx2 + vy2s*(cb*sa)
       vx2 = vx2 - vz2s*sb
-*     
+*
       vy2 = vx2s*(sg*sb*ca - cg*sa)
       vy2 = vy2 + vy2s*(sg*sb*sa + cg*ca)
       vy2 = vy2 + vz2s*(cb*sg)
-*     
+*
       vz2 = vx2s*(cg*sb*ca + sg*sa)
       vz2 = vz2 + vy2s*(cg*sb*sa - sg*ca)
       vz2 = vz2 + vz2s*(cb*cg)
 **
 **
       vx3 = vx3s*(cb*ca)
-      vx3 = vx3 + vy3s*(cb*sa) 
+      vx3 = vx3 + vy3s*(cb*sa)
       vx3 = vx3 - vz3s*sb
-*     
+*
       vy3 = vx3s*(sg*sb*ca - cg*sa)
       vy3 = vy3 + vy3s*(sg*sb*sa + cg*ca)
       vy3 = vy3 + vz3s*(cb*sg)
-*     
+*
       vz3 = vx3s*(cg*sb*ca + sg*sa)
       vz3 = vz3 + vy3s*(cg*sb*sa - sg*ca)
       vz3 = vz3 + vz3s*(cb*cg)
