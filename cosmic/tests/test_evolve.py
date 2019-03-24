2c35653519abb6e2912f58786ec1649b076d15c1
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2019-03-24 13:15:50 -0400

    trying to get these tests all set up for py27 vs py3x and metallicity changes

--- cosmic/tests/test_evolve.py
+++ cosmic/tests/test_evolve.py
@@ -12,7 +12,7 @@ from cosmic.evolve import Evolve
 from cosmic.sample.initialbinarytable import InitialBinaryTable
 
 TEST_DATA_DIR = os.path.join(os.path.split(__file__)[0], 'data')
-INIT_CONDITIONS = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'initial_conditions_for_testing.hdf'), key='initC')
+INIT_CONDITIONS = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'initial_conditions_for_testing.hdf5'), key='initC')
 BPP_DF = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'unit_tests_results.hdf5'), key='bpp')
 BCM_DF = pd.read_hdf(os.path.join(TEST_DATA_DIR, 'unit_tests_results.hdf5'), key='bcm')
 BSEDict = {}
