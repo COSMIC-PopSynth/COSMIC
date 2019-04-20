d4c57928c2b94a1e586472d0b8aa287610bdc22c
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2019-04-20 15:49:44 -0400

    fixing dumb indentation error

--- cosmic/sample/sampler/independent.py
+++ cosmic/sample/sampler/independent.py
@@ -405,8 +405,8 @@ class Sample(object):
             while len(t_samp) < size:
                 rand_age = np.random.uniform(min(times),max(times),1)
                 age_prob = np.random.uniform(0,1,1)
-                    if age_prob < SFH_interp(rand_age):
-                        t_samp.append(rand_age)
+                if age_prob < SFH_interp(rand_age):
+                    t_samp.append(rand_age)
             # Convert from Gyr to Myr for BSE
             tphys = (max(times)-np.array(t_samp))*1000.0
             metallicity = met*np.ones(size)
