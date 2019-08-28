8ade6d543d172213a3c46a21ed67cf6991a25695
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2019-08-28 13:46:40 -0500

    Alright madea  python 2.7 non-compliant move

--- setup.py
+++ setup.py
@@ -65,9 +65,8 @@ else:
 cmdclass["sdist"] = sdist
 
 # read description
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md', 'rb') as f:
+    long_description = f.read().decode().strip()
 
 # -- dependencies -------------------------------------------------------------
 
