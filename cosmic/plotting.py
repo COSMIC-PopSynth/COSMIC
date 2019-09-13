cce9db4c218648924df844c44653b0e7556ed54d
AstroJeff <andrews@astro.columbia.edu>
2019-09-13 13:27:33 -0500

    Added documentation to plotting.py

--- cosmic/plotting.py
+++ cosmic/plotting.py
@@ -16,7 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with cosmic.  If not, see <http://www.gnu.org/licenses/>.
 
-
+'''`plotting`
+'''
 
 import numpy as np
 import matplotlib
