f81054d141446b1cad121febf880a06a576ea9d6
AstroJeff <andrews@astro.columbia.edu>
2019-09-28 11:48:25 -0500

    Fixed effective lifetime problem for stellar mixing

--- cosmic/src/mix.f
+++ cosmic/src/mix.f
@@ -14,6 +14,7 @@
       REAL*8 M0(2),M(2),AJ(2),ZPARS(20),bhspin(2)
       REAL*8 TSCLS(20),LUMS(10),GB(10),TMS1,TMS2,TMS3,TN
       REAL*8 M01,M02,M03,M1,M2,M3,AGE1,AGE2,AGE3,MC3,MCH
+      REAL*8 M_CORE_BGB_1,M_CORE_BGB_2,M_CORE_BGB_3
       PARAMETER(MCH=1.44D0)
 *
 *
@@ -37,12 +38,14 @@
       M1 = M(I1)
       AGE1 = AJ(I1)
       CALL star(K1,M01,M1,TMS1,TN,TSCLS,LUMS,GB,ZPARS)
+      M_CORE_BGB_1 = GB(9)
 *
 *       Obtain time scales for second star.
       M02 = M0(I2)
       M2 = M(I2)
       AGE2 = AJ(I2)
       CALL star(K2,M02,M2,TMS2,TN,TSCLS,LUMS,GB,ZPARS)
+      M_CORE_BGB_2 = GB(9)
 *
 *       Check for planetary systems - defined as HeWDs and low-mass WDs!
       IF(K1.EQ.10.AND.M1.LT.0.05)THEN
@@ -84,7 +87,9 @@ C      ENDIF
 *       be partial mixing.
          IF(K1.EQ.7) KW = 7
          CALL star(KW,M03,M3,TMS3,TN,TSCLS,LUMS,GB,ZPARS)
-         AGE3 = REJUV_FAC*TMS3*(AGE1*M1/TMS1 + AGE2*M2/TMS2)/M3
+         M_CORE_BGB_3 = GB(9)
+         AGE3 = REJUV_FAC*TMS3*(AGE1*M_CORE_BGB_1/TMS1 + &
+                    AGE2*M_CORE_BGB_2/TMS2)/M_CORE_BGB_3
       ELSEIF(ICASE.EQ.3.OR.ICASE.EQ.6.OR.ICASE.EQ.9)THEN
          MC3 = M1
          CALL gntage(MC3,M3,KW,ZPARS,M03,AGE3)
