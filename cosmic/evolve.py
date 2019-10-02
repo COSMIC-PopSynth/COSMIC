74faa0208d0bd6d33ef6bb376122837a4f39a0ed
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2019-10-02 11:53:01 -0400

    removed unused flags from PK and moved actual flags out to Params.ini file

--- cosmic/evolve.py
+++ cosmic/evolve.py
@@ -74,7 +74,8 @@ INITIAL_CONDITIONS_BSE_COLUMNS = ['neta', 'bwind', 'hewind', 'alpha1', 'lambdaf'
                              'eddfac', 'gamma', 'bdecayfac', 'bconst', 'ck', 
                              'windflag', 'qcflag', 'eddlimflag',
                              'fprimc_array', 'dtp', 'randomseed',
-                             'bhspinflag','bhspinmag', 'rejuv_fac', 'rejuvflag', 'htpmb']
+                             'bhspinflag','bhspinmag', 'rejuv_fac', 'rejuvflag', 'htpmb'
+                             'ST_cr', 'ST_tide']
 
 INITIAL_CONDITIONS_MISC_COLUMN = ['bin_num']
 
@@ -340,6 +341,8 @@ class Evolve(object):
                 _evolvebin.mixvars.rejuv_fac = f[83]
                 _evolvebin.flags.rejuvflag = f[84]
                 _evolvebin.flags.htpmb = f[85]
+                _evolvebin.flags.ST_cr = f[86]
+                _evolvebin.flags.ST_tide = f[87]
                 _evolvebin.cmcpass.using_cmc = 0
 
                 [bpp, bcm] = _evolvebin.evolv2([f[0],f[1]], [f[2],f[3]], f[4], f[5], f[6], f[7], f[79],
@@ -359,8 +362,8 @@ class Evolve(object):
                     raise Warning('bpp overload: mass1 = {0}, mass2 = {1}, porb = {2}, ecc = {3}, tphysf = {4}, metallicity = {5}'\
                                    .format(f[2], f[3], f[4], f[5], f[7], f[6]))
 
-                bpp_bin_numbers = np.atleast_2d(np.array([f[86]] * len(bpp))).T
-                bcm_bin_numbers = np.atleast_2d(np.array([f[86]] * len(bcm))).T
+                bpp_bin_numbers = np.atleast_2d(np.array([f[88]] * len(bpp))).T
+                bcm_bin_numbers = np.atleast_2d(np.array([f[88]] * len(bcm))).T
 
                 bpp = np.hstack((bpp, bpp_bin_numbers))
                 bcm = np.hstack((bcm, bcm_bin_numbers))
@@ -425,6 +428,8 @@ class Evolve(object):
                     _evolvebin.mixvars.rejuv_fac = f[83]
                     _evolvebin.flags.rejuvflag = f[i,84]
                     _evolvebin.flags.htpmb = f[85]
+                    _evolvebin.flags.ST_cr = f[86]
+                    _evolvebin.flags.ST_tide = f[87]
                     _evolvebin.cmcpass.using_cmc = 0 
                     [bpp, bcm] = _evolvebin.evolv2([f[i,0],f[i,1]], [f[i,2],f[i,3]], f[i,4], f[i,5], f[i,6], f[i,7], f[i,79],
                                                     [f[i,8],f[i,9]], [f[i,10],f[i,11]], [f[i,12],f[i,13]],
@@ -443,8 +448,8 @@ class Evolve(object):
                         bcm = bcm[:np.argwhere(bcm[:,0] > 0)[0][0]]
                         raise Warning('bpp overload: mass1 = {0}, mass2 = {1}, porb = {2}, ecc = {3}, tphysf = {4}, metallicity = {5}'\
                                        .format(f[i,2], f[i,3], f[i,4], f[i,5], f[i,7], f[i,6]))
-                    bpp_bin_numbers = np.atleast_2d(np.array([f[i,86]] * len(bpp))).T
-                    bcm_bin_numbers = np.atleast_2d(np.array([f[i,86]] * len(bcm))).T
+                    bpp_bin_numbers = np.atleast_2d(np.array([f[i,88]] * len(bpp))).T
+                    bcm_bin_numbers = np.atleast_2d(np.array([f[i,88]] * len(bcm))).T
 
                     res_bpp[i] = np.hstack((bpp, bpp_bin_numbers))
                     res_bcm[i] = np.hstack((bcm, bcm_bin_numbers))
