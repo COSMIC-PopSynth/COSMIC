0cd29ab03fb9fa1e817bc348c8f21ef16cb55a09
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2020-03-10 12:19:39 -0400

    changing lower mass limits for exploding stars

--- cosmic/utils.py
+++ cosmic/utils.py
@@ -453,9 +453,9 @@ def mass_min_max_select(kstar_1, kstar_2):
     ii = 0
     for k in kstar_lo:
         if k == 14.0:
-            min_mass[ii] = 10.0
+            min_mass[ii] = 8.0
         elif k == 13.0:
-            min_mass[ii] = 6.0
+            min_mass[ii] = 3.0
         elif k == 12.0:
             min_mass[ii] = 1.0
         elif k == 11.0:
