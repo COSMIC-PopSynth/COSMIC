0c33ba5c78326d9ebe34588172877b16c5b91098
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-07-31 13:53:39 -0500

    Alright, we can make the Singles and Binaries table for CMC from cosmic

--- cosmic/sample/sampler/cmc.py
+++ cosmic/sample/sampler/cmc.py
@@ -25,14 +25,14 @@ from .sampler import register_sampler
 from .independent import Sample
 from .. import InitialCMCTable, InitialBinaryTable
 from ..cmc import elson
-from ...evolve import Evolve
+from ... import evolve
 
 __author__ = 'Scott Coughlin <scott.coughlin@ligo.org>'
 __credits__ = ['Newlin Weatherford <newlinweatherford2017@u.northwestern.edu>', 'Carl Rodriguez <carllouisrodriguez@gmail.com>']
 __all__ = ['get_cmc_sampler', 'CMCSample']
 
 
-def get_cmc_sampler(primary_model, ecc_model, porb_model, SF_start, SF_duration, binfrac_model, met, size, **kwargs):
+def get_cmc_sampler(primary_model, ecc_model, porb_model, binfrac_model, met, size, **kwargs):
     """Generates an initial binary sample according to user specified models
 
     Parameters
@@ -45,12 +45,6 @@ def get_cmc_sampler(primary_model, ecc_model, porb_model, SF_start, SF_duration,
 
     porb_model : `str`
         Model to sample orbital period; choices include: log_uniform, sana12
-
-    SF_start : `float`
-            Time in the past when star formation initiates in Myr
-
-    SF_duration : `float`
-            Duration of constant star formation beginning from SF_Start in Myr
 
     binfrac_model : `str or float`
         Model for binary fraction; choices include: vanHaaften or a fraction where 1.0 is 100% binaries
@@ -88,8 +82,7 @@ def get_cmc_sampler(primary_model, ecc_model, porb_model, SF_start, SF_duration,
     n_singles = 0
     n_binaries = 0
     mass1, total_mass1 = initconditions.sample_primary(primary_model, size=size)
-    breakpoint()
-    mass1_binaries, mass_single, binfrac_binaries = initconditions.binary_select(mass1, binfrac_model=binfrac_model)
+    mass1_binaries, mass_single, binfrac_binaries, binary_index = initconditions.binary_select(mass1, binfrac_model=binfrac_model)
     mass2_binaries = initconditions.sample_secondary(mass1_binaries)
 
     # track the mass sampled
@@ -107,24 +100,30 @@ def get_cmc_sampler(primary_model, ecc_model, porb_model, SF_start, SF_duration,
     binfrac = np.asarray(binfrac_binaries)
     ecc =  initconditions.sample_ecc(ecc_model, size = mass1_binary.size)
     porb =  initconditions.sample_porb(mass1_binary, mass2_binary, ecc, porb_model, size=mass1_binary.size)
-    tphysf, metallicity = initconditions.sample_SFH(SF_start=SF_start, SF_duration=SF_duration, met=met, size = mass1_binary.size)
-    metallicity[metallicity < 1e-4] = 1e-4
-    metallicity[metallicity > 0.03] = 0.03
     kstar1 = initconditions.set_kstar(mass1_binary)
     kstar2 = initconditions.set_kstar(mass2_binary)
 
-    # sample radius /obtain radius
-    Reff = initconditions.set_reff(mass1, **kwargs)
+    # obtain radius
+    Reff = initconditions.set_reff(mass1, metallicity=met, **kwargs)
+    Reff1 = Reff[binary_index]
+    Reff2 = initconditions.set_reff(mass2_binary, metallicity=met, **kwargs)
 
     # set radial velocity, set transverse velocity, set location in cluster
     vr, vt, r = initconditions.set_vr_vt_r(N=mass1.size, **kwargs)
-    breakpoint()
 
-    return InitialCMCTable.InitialCMCSingles(np.arange(mass1.size), initconditions.set_kstar(mass1), mass1, Reff, r, vr, vt, np.ones_like(mass1)), InitialCMCBinaries(cls, index, id1, initconditions.set_kstar(mass1), mass1, Reff1, id2, k2, m2, Reff2, a, e)
+    # set singles id
+    single_ids = np.arange(mass1.size)
+    binary_secondary_object_id = np.arange(mass1.size, mass1.size + mass2_binaries.size)
+
+    # set binind
+    binind = np.zeros(mass1.size)
+    binind[binary_index] = 1
+
+    return InitialCMCTable.InitialCMCSingles(single_ids, initconditions.set_kstar(mass1), mass1, Reff, r, vr, vt, binind), InitialCMCTable.InitialCMCBinaries(np.arange(mass1_binaries.size), single_ids[binary_index], kstar1, mass1_binary, Reff1, binary_secondary_object_id, kstar2, mass2_binary, Reff2, porb, ecc)
 
 
 register_sampler('cmc', InitialCMCTable, get_cmc_sampler,
-                 usage="binfrac_model, primary_model, ecc_model, SFH_model, component_age, metallicity, size")
+                 usage="primary_model ecc_model porb_model binfrac_model met size")
 
 
 class CMCSample(Sample):
@@ -141,13 +140,13 @@ class CMCSample(Sample):
 
         return vr, vt, r
 
-    def set_reff(self, mass, **kwargs):
+    def set_reff(self, mass, metallicity, **kwargs):
         # NUMBER 1: PASS A DICTIONARY OF FLAGS
         BSEDict = kwargs.pop('BSEDict', {})
 
         # NUMBER 2: PASS PATH TO A INI FILE WITH THE FLAGS DEFINED
         params = kwargs.pop('params', None)
 
-        bpp, bcm, initial_conditions, kick_info = Evolve.evolve(InitialBinaryTable.InitialBinaries(mass, np.ones_like(mass)*0, np.ones_like(mass)*1000000, np.ones_like(mass)*0, np.ones_like(mass)*0.1, self.set_kstar(mass), np.ones_like(mass)*0, np.ones_like(mass)*0.014), BSEDict=BSEDict, params=params)
+        bpp, bcm, initial_conditions, kick_info = evolve.Evolve.evolve(InitialBinaryTable.InitialBinaries(mass, np.ones_like(mass)*0, np.ones_like(mass)*1000000, np.ones_like(mass)*0, np.ones_like(mass)*0.1, self.set_kstar(mass), np.ones_like(mass)*0, np.ones_like(mass)*metallicity), BSEDict=BSEDict, params=params)
 
         return bcm.groupby('bin_num').first().rad_1
