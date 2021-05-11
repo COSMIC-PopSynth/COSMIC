f535eba3ffc61816fe4598e49707bffe329fc233
Katie Breivik <katelynbreivik2013@u.northwestern.edu>
2021-05-11 17:22:46 -0400

    it makes more sense to not include sep > 0 in this filtering (#519)
    * it makes more sense to not include sep > 0 in this filtering
    
    * also fixing problem with supplying params to initconditions

--- cosmic/utils.py
+++ cosmic/utils.py
@@ -226,7 +226,6 @@ def conv_select(bcm_save, bpp_save, final_kstar_1, final_kstar_2, method, conv_l
         conv_save = bpp_save.loc[
             (bpp_save.kstar_1.isin(final_kstar_1))
             & (bpp_save.kstar_2.isin(final_kstar_2))
-            & (bpp_save.sep > 0)
             & (bpp_save.evol_type.isin([2.0, 4.0]))
         ]
 
