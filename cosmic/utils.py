bdf9f58ccfd1d3709fc17b033796706f387ec776
Poojan <34255053+poojanagrawal@users.noreply.github.com>
2022-04-29 14:59:17 -0400

    Sse changes (#555)
    * created zsun_wind and set use_2to3 to False
    
    * Extended Vink winds for Teff.gt. 50000K, added Z dependence for Vinf/Vesc in Vink winds
    
    * updating unit tests which broke as we expected because the wind mass loss is just a bit stronger with the vink fits
    
    * added in some warnings to docs, inifile, errorcheck to flag when zsun is different from zsun_wind
    
    Co-authored-by: poojanagrawal <poojan@poojans-mbp.myfiosgateway.com>
    Co-authored-by: katiebreivik <katelynbreivik2013@u.northwestern.edu>

--- cosmic/utils.py
+++ cosmic/utils.py
@@ -960,6 +960,13 @@ def error_check(BSEDict, filters=None, convergence=None, sampling=None):
 
     flag = "zsun"
     if flag in BSEDict.keys():
+        if BSEDict[flag] != 0.019:
+            warnings.warn(
+                "'{0:s}' is set to a different value than assumed in the mlwind "
+                "prescriptions (you set it to '{1:0.2f}' and in mlwind, zsun_wind=0.019)".format(
+                    flag, BSEDict[flag]
+                )
+            )
         if BSEDict[flag] <= 0:
             raise ValueError(
                 "'{0:s}' needs to be greater than 0 (you set it to '{1:0.2f}')".format(
