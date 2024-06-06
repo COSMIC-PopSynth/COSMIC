66cc712c1fbb4b3a51ed038f5d7c2e8f51e1169a
poojanagrawal <poojanagrawal7@gmail.com>
2024-06-06 16:08:46 -0400

    METISSE now returns error instead of stopping the code

--- src/cosmic/src/zcnsts.f
+++ src/cosmic/src/zcnsts.f
@@ -4,11 +4,13 @@
       
       real*8 z,zpars(20)
       CHARACTER*256 path_to_tracks,path_to_he_tracks
-
+      integer :: ierr
 
       if (using_METISSE) then
           !WRITE(*,*) 'Calling METISSE_zcnsts',using_METISSE
-          CALL METISSE_zcnsts(z,zpars,path_to_tracks,path_to_he_tracks)
+          CALL METISSE_zcnsts(z,zpars,path_to_tracks,
+     &     path_to_he_tracks,ierr)
+           if (ierr/=0) call assign_error()
           
       elseif (using_SSE) then
           !WRITE(*,*) 'Calling SSE_zcnsts'
