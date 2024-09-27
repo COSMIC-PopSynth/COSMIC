a239d4d22b577f29bec14461df36f9f1c1c91b2a
Tom Wagg <tomjwagg@gmail.com>
2024-09-27 15:24:44 -0700

    add cross product function

--- src/cosmic/src/kick_but_better.f
+++ src/cosmic/src/kick_but_better.f
@@ -43,3 +43,19 @@
     
           RETURN
           END
+
+
+      FUNCTION CrossProduct(A, B, C)
+* This function computes the cross product of two vectors A and B
+* A, B are input vectors of dimension 3
+* C is the resulting vector, also of dimension 3
+
+      DOUBLE PRECISION A(3), B(3), C(3)
+
+* Calculate each component of the cross product
+      C(1) = A(2) * B(3) - A(3) * B(2)
+      C(2) = A(3) * B(1) - A(1) * B(3)
+      C(3) = A(1) * B(2) - A(2) * B(1)
+
+      RETURN
+      END
