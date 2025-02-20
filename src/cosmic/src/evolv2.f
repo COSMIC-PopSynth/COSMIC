f8a9fd6d219cb72273baced0f1e770833d968b23
aliperego <alypeggy313@gmail.com>
2025-02-20 15:16:15 +0100

    fix_WDrad

--- src/cosmic/src/evolv2.f
+++ src/cosmic/src/evolv2.f
@@ -2448,6 +2448,37 @@ component.
 
 *
          evolve_type = 8.0
+         
+         age = tphys - epoch(1)
+         mc = massc(1)
+         rc = radc(1)
+         CALL star(kstar(1),mass0(1),mass(1),tm,tn,tscls,lums,GB,zpars)
+         CALL hrdiag(mass0(1),age,mass(1),tm,tn,tscls,lums,GB,zpars,
+     &               rm,lum,kstar(1),mc,rc,me,re,k2,bhspin(1),1)
+         aj(1) = age
+         epoch(1) = tphys - age
+         rad(1) = rm
+         lumin(1) = lum  
+         massc(1) = mc
+         radc(1) = rc
+         menv(1) = me
+         renv(1) = re
+         
+         age = tphys - epoch(2)
+         mc = massc(2)
+         rc = radc(2)
+         CALL star(kstar(2),mass0(2),mass(2),tm,tn,tscls,lums,GB,zpars)
+         CALL hrdiag(mass0(2),age,mass(2),tm,tn,tscls,lums,GB,zpars,
+     &               rm,lum,kstar(2),mc,rc,me,re,k2,bhspin(2),2)
+         aj(2) = age
+         epoch(2) = tphys - age
+         rad(2) = rm
+         lumin(2) = lum  
+         massc(2) = mc
+         radc(2) = rc
+         menv(2) = me
+         renv(2) = re
+
          mass1_bpp = mass(1)
          mass2_bpp = mass(2)
          if(kstar(1).eq.15) mass1_bpp = mass0(1)
