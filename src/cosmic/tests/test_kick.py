dd3202d75593a00ab761e4e841710081b9986876
d-maclean <dmaclean@unc.edu>
2025-06-30 11:32:02 -0400

    changed BSEDict['acc_lim'] to list to satisfy COSMIC-METISSE

--- src/cosmic/tests/test_kick.py
+++ src/cosmic/tests/test_kick.py
@@ -27,7 +27,7 @@ BSEDict['fprimc_array'] = [2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,
                            2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0,2.0/21.0]
 BSEDict['grflag'] = 1
 BSEDict['don_lim'] = -1
-BSEDict['acc_lim'] = -1
+BSEDict['acc_lim'] = [-1,-1]
 BSEDict['wd_mass_lim'] = 0
 
 # avoid using the same randomseed
