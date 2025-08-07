26a3b4b66c2d16254f7534c203c8eca869fdc49f
d-maclean <dmaclean@unc.edu>
2025-08-07 17:38:53 -0400

    capture and recycle zpars after initial run

--- src/cosmic/evolve.py
+++ src/cosmic/evolve.py
@@ -31,6 +31,7 @@ import pandas as pd
 import warnings
 import os
 import sys
+from functools import partial
 try:
     import multiprocessing
     multiprocessing.set_start_method("fork")
@@ -440,6 +441,10 @@ class Evolve(object):
             initial_conditions[i]["n_col_bcm"] = len(bcm_columns)
             initial_conditions[i]["col_inds_bcm"] = col_inds_bcm
 
+        # evolve and discard a single system to
+        # load tracks and get zpars
+        _, _, _, _, _, zpars = _evolve_single_system(initial_conditions[0], None)
+                    
         # check if a pool was passed
         if pool is None:
             with MultiPool(processes=nproc) as pool:
@@ -455,7 +460,8 @@ class Evolve(object):
                         itr_block = itr_next
                     output = list(pool.map(_evolve_multi_system, initial_conditions_blocked))
                 else:
-                    output = list(pool.map(_evolve_single_system, initial_conditions))
+                    single_fargs = partial(_evolve_single_system, zpars=zpars)
+                    output = list(pool.map(single_fargs, initial_conditions))
         else:
             # evolve systems
             if n_per_block > 0:
@@ -469,7 +475,8 @@ class Evolve(object):
                     itr_block = itr_next
                 output = list(pool.map(_evolve_multi_system, initial_conditions_blocked))
             else:
-                output = list(pool.map(_evolve_single_system, initial_conditions))
+                single_fargs = partial(_evolve_single_system, zpars=zpars)
+                output = list(pool.map(single_fargs, initial_conditions))
 
         output = np.array(output, dtype=object)
         bpp_arrays = np.vstack(output[:, 1])
@@ -511,9 +518,11 @@ class Evolve(object):
         return bpp, bcm, initialbinarytable, kick_info
 
 
-def _evolve_single_system(f):
+def _evolve_single_system(f, zpars = None):
     try:
         f["kick_info"] = np.zeros((2, len(KICK_COLUMNS)-1))
+        if zpars is None:
+            zpars = np.zeros(20, dtype=np.float64)
         # determine if we already have a compact object, if yes than one SN has already occured
         if (f["kstar_1"] in range(10, 15)) or (f["kstar_2"] in range(10, 15)):
             f["kick_info"][0, 0] = 1
@@ -601,7 +610,7 @@ def _evolve_single_system(f):
         _evolvebin.col.n_col_bcm = f["n_col_bcm"]
         _evolvebin.col.col_inds_bcm = f["col_inds_bcm"]
 
-        [bpp_index, bcm_index, kick_info] = _evolvebin.evolv2([f["kstar_1"], f["kstar_2"]],
+        [zpars, bpp_index, bcm_index, kick_info] = _evolvebin.evolv2([f["kstar_1"], f["kstar_2"]],
                                                               [f["mass_1"], f["mass_2"]],
                                                               f["porb"], f["ecc"], f["metallicity"], 
                                                               f["tphysf"], f["dtp"],
@@ -620,9 +629,9 @@ def _evolve_single_system(f):
                                                               [f["tms_1"], f["tms_2"]],
                                                               [f["bhspin_1"], f["bhspin_2"]],
                                                               f["tphys"],
+                                                              zpars,
                                                               np.zeros(20),
-                                                              np.zeros(20),
-                                                              f["kick_info"])
+                                                              f["kick_info"])        
                                                               
         if bpp_index<0:
             raise ValueError("Failed in METISSE_zcnsts")
@@ -636,7 +645,7 @@ def _evolve_single_system(f):
             bcm = np.hstack((bcm, np.ones((bcm.shape[0], 1))*f["bin_num"]))
             kick_info = np.hstack((kick_info, np.ones((kick_info.shape[0], 1))*f["bin_num"]))
 
-        return f, bpp, bcm, kick_info, _evolvebin.snvars.natal_kick_array.copy()
+        return f, bpp, bcm, kick_info, _evolvebin.snvars.natal_kick_array.copy(), zpars
 
     except Exception as e:
         print(e)
@@ -644,6 +653,7 @@ def _evolve_single_system(f):
 
 
 def _evolve_multi_system(f):
+    zpars = None
     try:
         res_bcm = np.zeros(f.shape[0], dtype=object)
         res_bpp = np.zeros(f.shape[0], dtype=object)
@@ -652,7 +662,10 @@ def _evolve_multi_system(f):
         for i in range(0, f.shape[0]):
 
             # call evolve single system
-            _, bpp, bcm, kick_info, _ = _evolve_single_system(f[i])
+            if i == 0:
+                _, bpp, bcm, kick_info, _, zpars = _evolve_single_system(f[i], zpars)
+            else:
+                _, bpp, bcm, kick_info, _, _ = _evolve_single_system(f[i], zpars)
 
             # add results to pre-allocated list
             res_bpp[i] = bpp
