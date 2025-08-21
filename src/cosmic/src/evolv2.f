fc2031836a423130ea269b1138c10fe6000f5033
Katie Breivik <kbreivik@andrew.cmu.edu>
2025-08-21 10:59:27 -0400

    removing debug prints

--- src/cosmic/src/evolv2.f
+++ src/cosmic/src/evolv2.f
@@ -424,10 +424,8 @@ component.
          rc = radc(k)
          CALL star(kstar(k),mass0(k),mass(k),tm,tn,tscls,lums,GB,zpars,
      &                                                          dtm,k)
-         WRITE(*,*) 'star 427: ',kstar(k), mass(k), tm, tn
          CALL hrdiag(mass0(k),age,mass(k),tm,tn,tscls,lums,GB,zpars,
      &               rm,lum,kstar(k),mc,rc,me,re,k2,bhspin(k),k)
-         WRITE(*,*) 'hrdiag 430: ',kstar(k), mass(k), rm, lum, mc, rc
          aj(k) = age
          epoch(k) = tphys - age
          rad(k) = rm
