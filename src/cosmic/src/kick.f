10892f0944b013ce00ab3b323f2a61771c4bc818
Tom Wagg <tomjwagg@gmail.com>
2026-01-13 15:40:52 -0500

    draw from gaussian properly

--- src/cosmic/src/kick.f
+++ src/cosmic/src/kick.f
@@ -232,8 +232,9 @@
              else
                 mu_mm = mm_mu_ns * max(m1c - m1n, 0.0d0) / m1n
              endif
-             call RandomTruncatedNormal(mu_mm, 0.3d0 * mu_mm, idum1,
-     &                                  0.d0, 10000.d0, vk)
+             call RandomTruncatedNormal(0, 0.3d0, idum1,
+     &                                  -1.d0, 10000.d0, vk)
+             vk = mu_mm * (1 + vk)
              vk2 = vk * vk
           else
 * Otherwise use the Hobbs et al. 2005 Maxwellian distribution
