987488646702ee98bd67a2bfb1dbfbf49fd22b87
Tom Wagg <tomjwagg@gmail.com>
2026-02-18 13:55:19 -0500

    only show recent version changes

--- docs/create_settings_html.py
+++ docs/create_settings_html.py
@@ -12,6 +12,21 @@ import json
 import pandas as pd
 from cosmic import __version__ as cosmic_version
 
+# how many versions in the past should we show as badges for when options were added?
+VERSION_CUTOFFS = {
+    "major": 0,
+    "minor": 3,
+    "patch": 1000
+}
+
+def version_is_recent(version_string):
+    version_nums = version_string.split(".")
+    cosmic_nums = cosmic_version.split(".")
+    for i, label in enumerate(["major", "minor", "patch"]):
+        if int(cosmic_nums[i]) - int(version_nums[i]) > VERSION_CUTOFFS[label]:
+            return False
+    return True
+
 # blame BS4 for me calling this soup
 main_soup = """<!-- This file should be created using create_settings_html.py -->
 <div class="container-fluid"></div>
@@ -97,8 +112,9 @@ for group in settings:
         
         # add the version added if it's there
         if "version_added" in setting:
-            new_setting.select_one(".version-added").append(bs4.BeautifulSoup(
-                f"""<span class="badge badge-success version-added"><a class="link-white" href="https://github.com/COSMIC-PopSynth/COSMIC/releases/tag/v{setting['version_added']}">Added in v{setting['version_added']}</a></span>""", 'html.parser'))
+            if version_is_recent(setting["version_added"]):
+                new_setting.select_one(".version-added").append(bs4.BeautifulSoup(
+                    f"""<span class="badge badge-success version-added"><a class="link-white" href="https://github.com/COSMIC-PopSynth/COSMIC/releases/tag/v{setting['version_added']}">Added in v{setting['version_added']}</a></span>""", 'html.parser'))
 
         # colour the sublinks the same as the border of the group
         new_setting.select_one(".options-expander")["style"] = "color: " + group["docs-colour"] + ";"
@@ -173,9 +189,10 @@ for group in settings:
             new_option_expl.select_one(".opt-desc").append(bs4.BeautifulSoup(option["description"], 'html.parser'))
 
             if "version_added" in option:
-                new_option_expl.select_one(".opt-badge-cont").append(bs4.BeautifulSoup(
-                    f"""<span class="badge badge-success version-added"><a class="link-white" href="https://github.com/COSMIC-PopSynth/COSMIC/releases/tag/v{option['version_added']}">Added in v{option['version_added']}</a></span>""", 'html.parser'
-                ))
+                if version_is_recent(option["version_added"]):
+                    new_option_expl.select_one(".opt-badge-cont").append(bs4.BeautifulSoup(
+                        f"""<span class="badge badge-success version-added"><a class="link-white" href="https://github.com/COSMIC-PopSynth/COSMIC/releases/tag/v{option['version_added']}">Added in v{option['version_added']}</a></span>""", 'html.parser'
+                    ))
             new_setting.select_one(".options").ul.append(new_option_expl)
 
         # convert the default options to a string and display it
