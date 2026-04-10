dcf1c342059253b0d9f1b8618e9f6fb9dcc28323
Katie Breivik <kbreivik@andrew.cmu.edu>
2026-04-10 17:15:12 -0400

    fixed the segfaults -- it was an argument order for assign_remnant calls

--- src/cosmic/src/const_bse.h
+++ src/cosmic/src/const_bse.h
@@ -14,7 +14,7 @@
       COMMON /FLAGS/ tflag,ifflag,remnantflag,wdflag,bhflag,windflag,
      &               qcflag,eddlimflag,bhspinflag,aic,rejuvflag,
      &               htpmb,ST_cr,ST_tide,bdecayfac,grflag,
-     &               bhms_coll_flag,wd_mass_lim,rtmsflag
+     &               bhms_coll_flag,wd_mass_lim,rtmsflag,maltsev_mode
       REAL*8 don_lim,acc_lim(2),Mbh_initial
       COMMON /MTVARS/ don_lim,acc_lim,Mbh_initial
       INTEGER ceflag,cekickflag,cemergeflag,cehestarflag,ussn
